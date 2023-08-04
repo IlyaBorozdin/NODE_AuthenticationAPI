80bc830d3a6826593e84ed6a6315c5a5ad8a6dbc
IlyaBorozdin <borozdin.ilshat2004@gmail.com>
2023-08-04 19:08:32 +0300

    продолжаем проект

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const https = require('https');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
+const shutdown = require('./src/shutdown');
+
+const debugMode = process.env.DEBUG_MODE === 'true' || false;
+const logLevel = process.env.LOG_LEVEL || 'info';
+
 const app = express();
+
+app.use(express.json());
 
 const keyPath = path.join(__dirname, 'certificates', 'localhost.key');
 const certPath = path.join(__dirname, 'certificates', 'localhost.crt');
 
-const options = {
-    key: fs.readFileSync(keyPath),
-    cert: fs.readFileSync(certPath)
-};
+Promise.all([
+    fs.readFile(keyPath, 'utf8'),
+    fs.readFile(certPath, 'utf8')
+])
+    .then(([keyData, certData]) => {
+        const options = { key: keyData, cert: certData };
 
-const port = 443;
+        const port = process.env.PORT || 443;
 
-const server = https.createServer(options, app);
+        const server = https.createServer(options, app);
 
-server.listen(port, () => {
-    console.log(`Server is running on https on port ${port}`);
-});
+        server.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+
+        shutdown(server);
+    })
+    .catch(err => {
+        console.error('Failed to start server', err);
+    });
---
+++ src/shutdown.js
@@ -0,0 +1,40 @@
+const gracefulShutdown = require('graceful-shutdown-express');
+
+const exit = (exitCode) => {
+    console.log('Server is closed');
+    process.exit(exitCode);
+};
+
+function shutdown(server) {
+    process.on('uncaughtException', (err) => {
+        console.error('Uncaught exception:', err);
+        gracefulShutdown(server, () => {
+            exit(1);
+        });
+    });
+    
+    process.on('unhandledRejection', (reason) => {
+        console.error('Unhandled rejection, reason:', reason);
+        gracefulShutdown(server, () => {
+            exit(1);
+        });
+    });
+    
+    process.on('SIGINT', () => {
+        console.log('Server is shutting down');
+        gracefulShutdown(server, {
+            timeout: 30000,
+            forceExit: true,
+            onShutdown: (err) => {
+                if (err) {
+                    console.error('Error during server shutdown:', err);
+                    exit(1);
+                } else {
+                    exit(0);
+                }
+            },
+        });
+    });
+}
+
+module.exports = shutdown;
